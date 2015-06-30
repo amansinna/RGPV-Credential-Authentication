1ec0bcbb2d2c9c69365785e7da9631cca6c2ea8b
amansinhaitm <amansinhaitm@gmail.com>
2015-06-30 22:44:51 +0530

    Modified

--- index.php
+++ index.php
@@ -6,18 +6,17 @@ $rgpv = file_get_html('https://www.rgpv.ac.in/');
 //Fetching ViewState
 foreach($rgpv->find('[id=__VIEWSTATE]') as $divs)
 $viewState = $divs->value;
+$viewState = urlencode($viewState);
 
 //Credentials
-$username=$_GET['username']; 
-$password=$_GET['password']; 
+$username="0905cs131019";
+$password="!@aman12"; 
 
-if(isset($_GET['username']) && isset($_GET['password']))
-{
+
 //Curl Header
 $url="https://www.rgpv.ac.in/Index.aspx"; 
 $cookie="ASP.NET_SessionId=q0it2t45ti0acj45nwifwp45; __utma=165311676.2106723673.1435573317.1435573317.1435573317.1; __utmb=165311676.2.10.1435573317; __utmc=165311676; __utmz=165311676.1435573317.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt=1"; 
-$postdata = 'ScriptManager1=UserLogin1$UpdPnlLogin|UserLogin1$btnLogin&__LASTFOCUS=&__EVENTTARGET=&__EVENTARGUMENT=&__VIEWSTATE='.$viewState.'&UserLogin1$txtUserName='.$username.'&UserLogin1$txtPassword='.$password.'&UserLogin1$btnLogin.x=15&UserLogin1$btnLogin.y=9';
-
+$postdata = 'ScriptManager1=UserLogin1$UpdPnlLogin|UserLogin1$btnLogin&__LASTFOCUS=&__EVENTTARGET=&__EVENTARGUMENT=&__VIEWSTATE='.$viewState.'&UserLogin1$txtUserName='.$username.'&UserLogin1$txtPassword='.$password.'&UserLogin1$btnLogin.x=26&UserLogin1$btnLogin.y=11';
 $ch = curl_init(); 
 curl_setopt ($ch, CURLOPT_URL, $url); 
 curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, FALSE);  
@@ -39,7 +38,7 @@ else
 		echo 'fatalError';
 else
 	echo 'false';
-//echo $result;  
+echo $result;  
 curl_close($ch);
-}
+
 ?>
